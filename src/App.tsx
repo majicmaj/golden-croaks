022b472851fbedbf44539768d857bece70793239
majicmaj <majd.tarbin@outlook.com>
2025-01-23 17:11:03 -0500

    Replace NameNavButton with a select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import { Link, Outlet } from 'react-router';
 import './App.css';
 import { useState } from 'react';
-import NameNavButton from './component/NameNavButton';
 
 const NAMES = ['majd', 'tyler', 'bert'];
 function App() {
   const [name, setName] = useState('bert');
 
-  const handleClick = (name: string) => setName(name);
-
   return (
     <div className="h-full w-full">
       <div className="absolute croak-bg" />
       <div className="p-4 flex flex-col gap-2 absolute h-full w-full">
-        <div className="flex gap-4">
-          {NAMES.map((name) => (
-            <NameNavButton key={name} name={name} handleClick={handleClick} />
-          ))}
-        </div>
+        <nav className="p-4 bg-gray-500/15 rounded flex gap-8 items-center">
+          <select
+            className="bg-gray-100 p-2 dark:bg-gray-500/15 rounded-md"
+            onChange={(e) => setName(e.target.value)}
+          >
+            {NAMES.map((name) => (
+              <option key={name} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
 
-        <nav className="p-4 bg-gray-500/15 rounded flex gap-8">
-          <p>Person: {name}</p>
           <Link to={`${name}/lesson/1`}>Lesson 1</Link>
           <Link to={`${name}/lesson/2`}>Lesson 2</Link>
         </nav>
