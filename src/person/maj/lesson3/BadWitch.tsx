2760f663435f5d7e3cc65150e91056925ad3486c
majicmaj <majd.tarbin@outlook.com>
2025-01-24 16:09:58 -0500

    Refactor BadWitch component to Card; enhance props for name, cost, type, description, and stats

--- src/person/maj/lesson3/BadWitch.tsx
+++ src/person/maj/lesson3/BadWitch.tsx
@@ -3,8 +3,8 @@ const URL =
 
 const Pill = ({ children }: { children: React.ReactNode }) => (
   // We make the pill 200% wide and then scale it back to 50% to make the border-radius shrink
-  <div className="grid w-[200%] scale-x-50 place-items-center rounded-full bg-slate-100/70 p-[0.5vmin] px-[5vmin]">
-    <div className="scale-x-[200%] font-bold">{children}</div>
+  <div className="grid w-[200%] scale-x-50 place-items-center rounded-full bg-slate-100/70 p-[0.5vmin] px-[3vmin]">
+    <div className="w-[50%] scale-x-[200%] font-bold">{children}</div>
   </div>
 );
 
@@ -25,28 +25,47 @@ const StatsBlock = ({ children }: { children: React.ReactNode }) => (
 const CardBase = ({ children }: { children: React.ReactNode }) => (
   <div className="grid place-items-center font-serif text-[2.5vmin] text-black">
     <div className="rounded-[2vmin] bg-black p-[2vmin]">
-      <div className="mb-[2vmin] flex aspect-[2/3] h-[80vmin] flex-col items-center gap-[0.5vmin] rounded-[1vmin] bg-gradient-to-r from-purple-800 to-teal-800 p-[2vmin]">
+      <div className="bg-rotate mb-[2vmin] flex aspect-[2/3] h-[80vmin] flex-col items-center gap-[0.5vmin] rounded-[1vmin] bg-gradient-to-r from-purple-800 to-teal-800 p-[2vmin]">
         {children}
       </div>
     </div>
   </div>
 );
 
-const BadWitch = () => {
+const Card = ({ name, cost, type, description, stats }) => {
   return (
     <CardBase>
-      <Pill>Bad Witch</Pill>
-      <img src={URL} className="px-[1vmin]" />
-      <Pill>Creature - Eldritch Witch</Pill>
-      <div className="relative flex-1 px-[1vmin]">
-        <div className="h-full w-full bg-slate-200 p-[1vmin] text-[2.25vmin]">
-          <ManaCost>1</ManaCost> Eldritch Blast: Deal 1 damage to target
-          creature or player.
+      <Pill>
+        <div className="flex items-center justify-between gap-[1vmin]">
+          <span>{name}</span>
+          <ManaCost>{cost}</ManaCost>
         </div>
-        <StatsBlock>3/2</StatsBlock>
+      </Pill>
+      <img src={URL} className="px-[1vmin]" />
+      <Pill>{type}</Pill>
+      <div className="relative flex-1 px-[1vmin]">
+        <div className="h-full w-full bg-slate-200/80 p-[1vmin] text-[2.25vmin]">
+          {description}
+        </div>
+        <StatsBlock>{stats}</StatsBlock>
       </div>
     </CardBase>
   );
 };
+
+const BadWitch = () => (
+  <Card
+    name="Bad Witch"
+    cost={3}
+    type="Creature - Eldritch Witch"
+    description={
+      <div>
+        <ManaCost>1</ManaCost> Eldritch Blast: Deal 1 damage to target creature
+        or player.
+      </div>
+    }
+    stats="3/2"
+  />
+);
 
 export default BadWitch;
