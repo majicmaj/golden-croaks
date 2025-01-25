1f7e2378ea0bbaab1786b942f8b5ee78bb2277e4
majicmaj <majd.tarbin@outlook.com>
2025-01-25 13:35:24 -0500

    Refactor getBoardCellColor function to improve comments and adjust color scheme for board cells

--- src/person/maj/lesson3/utils/getBoardCellColor.ts
+++ src/person/maj/lesson3/utils/getBoardCellColor.ts
@@ -3,7 +3,7 @@ export const getBoardCellColor = (
   j: number,
   selectedPosition: number[] | null
 ) => {
-  // if selected position is the same as the current position, return green
+  // Make selected cell green
   if (
     selectedPosition &&
     selectedPosition[0] === i &&
@@ -12,5 +12,6 @@ export const getBoardCellColor = (
     return 'bg-green-500';
   }
 
-  return (i + j) % 2 === 0 ? 'bg-slate-400' : 'bg-blue-800';
+  // Otherwise, alternate between dark blue and light blue
+  return (i + j) % 2 === 0 ? 'bg-blue-300' : 'bg-blue-900';
 };
