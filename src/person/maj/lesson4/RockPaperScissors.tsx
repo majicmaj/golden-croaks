bdced540f80d7dfd3734c8734736324b3f95d0c8
majicmaj <majd.tarbin@outlook.com>
2025-01-27 00:49:04 -0500

    Stop using index and use the value directly

--- src/person/maj/lesson4/RockPaperScissors.tsx
+++ src/person/maj/lesson4/RockPaperScissors.tsx
@@ -5,19 +5,16 @@ import { getRandomPick } from './utils/getRandomPick';
 const OPTIONS = ['Rock', 'Paper', 'Scissors'];
 
 const RockPaperScissors = () => {
-  const [playerOnePick, setPlayerOnePick] = useState<number | null>(null);
-  const [playerTwoPick, setPlayerTwopick] = useState<number | null>(null);
+  const [playerOnePick, setPlayerOnePick] = useState<string | null>(null);
+  const [playerTwoPick, setPlayerTwopick] = useState<string | null>(null);
 
   const [playerOneScore, setPlayerOneScore] = useState<number>(0);
   const [playerTwoScore, setPlayerTwoScore] = useState<number>(0);
 
-  const playerOneChoice = playerOnePick !== null && OPTIONS[playerOnePick];
-  const playerTwoChoice = playerTwoPick !== null && OPTIONS[playerTwoPick];
-
   const shoot = () => {
     if (playerOnePick === playerTwoPick) return; // Draw
 
-    const isWin = getResult(playerOneChoice, playerTwoChoice);
+    const isWin = getResult(playerOnePick, playerTwoPick);
 
     if (isWin) {
       setPlayerOneScore(playerOneScore + 1);
@@ -27,8 +24,8 @@ const RockPaperScissors = () => {
   };
 
   const handleClick = (i: number) => {
-    setPlayerOnePick(i);
-    setPlayerTwopick(getRandomPick());
+    setPlayerOnePick(OPTIONS[i]);
+    setPlayerTwopick(OPTIONS[getRandomPick()]);
     shoot();
   };
 
@@ -47,11 +44,11 @@ const RockPaperScissors = () => {
 
       <div className="grid flex-1 grid-cols-2 place-items-center py-4">
         <div className="flex flex-col items-center">
-          <p>{playerOneChoice}</p>
+          <p>{playerOnePick}</p>
           <p className="text-sm opacity-80">You</p>
         </div>
         <div className="flex flex-col items-center">
-          <p>{playerTwoChoice}</p>
+          <p>{playerTwoPick}</p>
           <p className="text-sm opacity-80">Computer</p>
         </div>
       </div>
