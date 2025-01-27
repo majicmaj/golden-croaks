0df50df4b79e75c1803791e48d4fe5726e99ec23
majicmaj <majd.tarbin@outlook.com>
2025-01-27 11:02:51 -0500

    Update Home to use select components for lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLocation, useNavigate } from 'react-router';
+import { Outlet, useLocation, useNavigate } from 'react-router';
 import './App.css';
 import { useState } from 'react';
 import majRoutes from './person/maj/majRoutes';
@@ -11,27 +11,35 @@ const LESSONS = {
   tyler: tylerRoutes,
   bert: bertRoutes,
 };
+
 function App() {
   const location = useLocation();
   const locationName = location.pathname.split('/')[1];
   const [name, setName] = useState(locationName || 'bert');
+  const [lesson, setLesson] = useState('');
 
   const navigate = useNavigate();
 
   const handleNameChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     navigate(`/${e.target.value}`);
     setName(e.target.value);
+    setLesson('');
+  };
+
+  const handleLessonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    navigate(`${name}/${e.target.value}`);
+    setLesson(e.target.value);
   };
 
   return (
     <div className="h-full w-full">
       <div className="croak-bg absolute" />
       <div className="absolute flex h-full w-full flex-col gap-2 p-4">
-        <nav className="flex items-center gap-8 rounded bg-gray-500/15 p-4">
+        <nav className="flex items-center gap-4 rounded bg-gray-500/15 p-4">
           <select
-            className="rounded-md bg-gray-100 p-2 dark:bg-gray-500/15"
-            value={name}
             onChange={handleNameChange}
+            value={name}
+            className="rounded-md bg-gray-100 p-2 dark:bg-gray-500/15"
           >
             {NAMES.map((name) => (
               <option key={name} value={name}>
@@ -40,13 +48,17 @@ function App() {
             ))}
           </select>
 
-          {LESSONS[name as keyof typeof LESSONS]
-            .filter((_, index) => index !== 0)
-            .map((_, index) => (
-              <Link key={index} to={`${name}/${index + 1}`}>
-                Lesson {index + 1}
-              </Link>
+          <select
+            onChange={handleLessonChange}
+            value={lesson}
+            className="rounded-md bg-gray-100 p-2 dark:bg-gray-500/15"
+          >
+            {LESSONS[name as keyof typeof LESSONS].map((_, index) => (
+              <option key={index} value={index === 0 ? '' : index}>
+                {index === 0 ? 'Home' : `Lesson ${index}`}
+              </option>
             ))}
+          </select>
         </nav>
 
         <div className="h-full rounded bg-gray-500/15 p-4">
