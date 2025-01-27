26c2a65abf422489e556fbf711a070cf91a0e2ee
Azrim9 <teegeh94@gmail.com>
2025-01-27 09:27:30 -0500

    includes HP reduction, winner and updated tailwindcss

--- src/person/bert/lesson3/RockPaperScissors.tsx
+++ src/person/bert/lesson3/RockPaperScissors.tsx
@@ -1,61 +1,99 @@
 import { useState } from 'react';
 
-const options= ["Rock", "Paper", "Scissors"]
+const OPTIONS= ["Rock", "Paper", "Scissors"]
 
 function RockPaperScissors() {
-const [playerOnePick, setPlayerOnePick] = useState("")
-const [playerTwoPick, setPlayerTwoPick] = useState("")
-const [result, setResult] = useState("")
+  const [playerOnePick, setPlayerOnePick] = useState("")
+  const [playerTwoPick, setPlayerTwoPick] = useState("")
+  const [playerOneHp, setPlayerOneHp] = useState(10)
+  const [playerTwoHp, setPlayerTwoHp] = useState(10)
+  const [winner, setWinner] = useState("")
+  const [playing, setPlaying] = useState(true)
 
+  const getPlayerTwoPick = () => {
+    const number = Math.floor(Math.random() * OPTIONS.length)
+    setPlayerTwoPick(OPTIONS[number])
+  }
 
-const getPlayerTwoPick = (options: string[]) => {
-    const number = Math.floor(Math.random() * options.length)
-    setPlayerTwoPick(options[number])
-}
-
-const playRock = (options: string[]) => {
-    setPlayerOnePick("Rock")
-    getPlayerTwoPick(options)
-}
-
-const playPaper = (options: string[]) => {
-    setPlayerOnePick("Paper")
-    getPlayerTwoPick(options)
-}
-
-const playScissors = (options: string[]) => {
-    setPlayerOnePick("Scissors") 
-    getPlayerTwoPick(options)
-}
-
-const picKWinner = () => {
-    if (playerOnePick === playerTwoPick) {setResult("Tie 👔")}  
+  const pickRoundWinner = () => {
+    if (playerOnePick === playerTwoPick) return
     else if (
         playerOnePick === "Rock" && playerTwoPick === "Scissors" || 
         playerOnePick === "Paper" && playerTwoPick === "Rock" ||
         playerOnePick === "Scissors" && playerTwoPick === "Paper"
-    ) { 
-        setResult("Player 1 wins 💅")}
-    else {
-        setResult("Player 2 wins 💀")}
-}
+    ) setPlayerOneHp(playerOneHp - 1)
+    else setPlayerTwoHp(playerTwoHp - 1)
+  }
 
+  const play = (pick: string) => {
+    pickRoundWinner()
+    pickGameWinner()
+    setPlayerOnePick(pick)
+    getPlayerTwoPick()
+    }
+
+  const pickGameWinner = () => {
+    if (playerOneHp === 0) {
+        setPlaying(false)
+        setWinner("Bro lost to Math.random 💀")
+        
+    }
+    else if (playerTwoHp === 0) {
+        setPlaying(false)
+        setWinner("Good ol fashioned win brah 🦍💯💪")
+        
+    }
+    else setPlaying(true)
+    }
+    
+
+  const resetGame = () => {
+    setPlayerOnePick("")
+    setPlayerTwoPick("")
+    setPlayerOneHp(10)
+    setPlayerTwoHp(10)
+    setWinner("")
+    setPlaying(true)
+  }
 
 return (
 <div>
-
-   
-    <button onClick={() => { playRock(options); picKWinner(); }}> Rock </button>
-    <button onClick={() => { playPaper(options); picKWinner(); }}>  Paper </button>
-    <button onClick={() => { playScissors(options); picKWinner(); }}>  Scissors </button>
-
-    <div>
-        <div> Player 1:{playerOnePick}</div>
-        <div> Player 2:{playerTwoPick}</div>
-        <div> Result:{result} </div>
+  {playing === false ? (
+   <div className="w-full h-full bg-slate-500">
+    <div className="text-center"> {winner ==="" ? "(Audience)👀👀👀" : winner} </div>
+    <div className="flex justify-between">
+     <div className="bg-blue-500 ml-2 border-2 pl-1 pr-1 rounded border-black font-semibold"> Player 1: {playerOnePick} HP: {playerOneHp} </div>
+     <div className="bg-red-500  mr-2 border-2 pl-1 pr-1 rounded border-black font-semibold"> Player 2: {playerTwoPick}  HP: {playerTwoHp} </div>
     </div>
-</div>
-);
+    <div className="flex justify-start ml-2">
+     {OPTIONS.map((option) => (
+        <button className="mt-4 pr-2 pl-2 mr-2 border rounded border-black font-semibold bg-yellow-300"> {option} </button>
+     ))}
+    </div>
+    <div className="flex justify-center">
+    <button className="mb-1 border-2 pl-1 pr-1 rounded border-black font-semibold bg-purple-600"  onClick={() => {resetGame()}}> Restart </button>
+    </div>
+</div> 
+) : (
+    <div className="w-full h-full bg-slate-500">
+    <div className="text-center"> (Audience)👀👀👀 </div>
+    <div className="flex justify-between">
+     <div className="bg-blue-500 ml-2 border-2 pl-1 pr-1 rounded border-black font-semibold"> Player 1: {playerOnePick} HP: {playerOneHp} </div>
+     <div className="bg-red-500  mr-2 border-2 pl-1 pr-1 rounded border-black font-semibold"> Player 2: {playerTwoPick}  HP: {playerTwoHp} </div>
+    </div>
+    <div className="flex justify-start ml-2">
+    {OPTIONS.map((option) => (
+        <button className="mt-4 pr-2 pl-2 mr-2 border rounded border-black font-semibold bg-yellow-300" onClick={() => {play(option)}}>{option}</button>
+     ))}
+    </div>
+    <div className="flex justify-center">
+        <button className="mb-1 border-2 pl-1 pr-1 rounded border-black font-semibold bg-purple-600"  onClick={() => {resetGame()}}> Restart </button>
+    </div>
+</div> 
+)}
 
+</div> 
+);
 }
+
 export default RockPaperScissors;
