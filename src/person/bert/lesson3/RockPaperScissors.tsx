d79806929a40aae1e6632999f560fc0a30691525
Azrim9 <teegeh94@gmail.com>
2025-01-31 07:11:39 -0500

    Improved readibly of pickRoundWinner function and updated css to include healthbars

--- src/person/bert/lesson3/RockPaperScissors.tsx
+++ src/person/bert/lesson3/RockPaperScissors.tsx
@@ -11,22 +11,22 @@ function RockPaperScissors() {
 
   const winner = getWinner(playerOneHp, playerTwoHp);
 
-  const pickRoundWinner = (a: string, b: string) => {
-    if (a === b) return;
+  const pickRoundWinner = (player1: string, player2: string) => {
+    if (player1 === player2) return;
     else if (
-      (a === 'Rock' && b === 'Scissors') ||
-      (a === 'Paper' && b === 'Rock') ||
-      (a === 'Scissors' && b === 'Paper')
+      (player1 === 'Rock' && player2 === 'Scissors') ||
+      (player1 === 'Paper' && player2 === 'Rock') ||
+      (player1 === 'Scissors' && player2 === 'Paper')
     )
-      setPlayerTwoHp(prev => prev - 1);
-    else setPlayerOneHp(prev => prev - 1);
+      setPlayerTwoHp((prev) => prev - 1);
+    else setPlayerOneHp((prev) => prev - 1);
   };
 
   const play = (pick: string) => {
-    if (winner) return
+    if (winner) return;
     setPlayerOnePick(pick);
     const pickTwo = getPlayerTwoPick();
-    getPlayerTwoPick();
+    setPlayerTwoPick(pickTwo);
     pickRoundWinner(pick, pickTwo);
   };
 
@@ -40,22 +40,43 @@ function RockPaperScissors() {
   return (
     <div>
       <div className="h-full w-full bg-slate-500">
-        <div className="text-center">
+        <div className="text-center font-semibold">
           {winner === null ? '(Audience)👀👀👀' : winner}
         </div>
         <div className="flex justify-between">
-          <div className="ml-2 rounded border-2 border-black bg-blue-500 pl-1 pr-1 font-semibold">
-            Player 1: {playerOnePick} HP: {playerOneHp}
+          <div className="ml-2 flex w-[140px] flex-col">
+            <div className="border-1 rounded border-2 border-black bg-gray-400 font-semibold">
+              {/* Using -mb-6 and z-10 to keep the PlayerOneHp on top of the green and grey divs */}
+              <div className="z-10 -mb-6 text-center">{playerOneHp}</div>
+              <div
+                className="h-[28px] rounded border-2 border-black bg-green-500"
+                style={{ width: playerOneHp * 10 + '%' }}
+              ></div>
+            </div>
+            <div className="rounded border-2 border-black bg-blue-500 pl-1 pr-1 text-center font-semibold">
+              Player 1: {playerOnePick}
+            </div>
           </div>
-          <div className="mr-2 rounded border-2 border-black bg-red-500 pl-1 pr-1 font-semibold">
-            Player 2: {playerTwoPick} HP: {playerTwoHp}
+          <div className="mr-2 flex w-[140px] flex-col">
+            <div className="border-1 rounded border-2 border-black bg-gray-400 font-semibold">
+              {/* Using -mb-6 and z-10 to keep the PlayerOneHp on top of the green and grey divs */}
+              <div className="z-10 -mb-6 text-center">{playerTwoHp}</div>
+              <div
+                className="h-[28px] rounded border-2 border-black bg-green-500"
+                style={{ width: playerTwoHp * 10 + '%' }}
+              ></div>
+            </div>
+            <div className="rounded border-2 border-black bg-red-500 pl-1 pr-1 text-center font-semibold">
+              Player 2: {playerTwoPick}
+            </div>
           </div>
         </div>
         <div className="ml-2 flex justify-start">
           {OPTIONS.map((option) => (
             <button
               className="mr-2 mt-4 rounded border border-black bg-yellow-300 pl-2 pr-2 font-semibold"
-              onClick={() => play(option)}>
+              onClick={() => play(option)}
+            >
               {option}
             </button>
           ))}
@@ -63,7 +84,8 @@ function RockPaperScissors() {
         <div className="flex justify-center">
           <button
             className="mb-1 rounded border-2 border-black bg-purple-600 pl-1 pr-1 font-semibold"
-            onClick={resetGame}>
+            onClick={resetGame}
+          >
             Restart
           </button>
         </div>
