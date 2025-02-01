aedd5984b18d1fabedc606dc44cc177877ecf609
GuyManDud <guitarskater1122@aol.com>
2025-02-01 16:12:32 -0500

    Fixed RyanHome and got rid of MajHome in Ryan Routing

--- src/person/ryguy/RyanRoutes.tsx
+++ src/person/ryguy/RyanRoutes.tsx
@@ -1,11 +1,11 @@
 import { Route } from 'react-router';
-import MajHome from './MajHome';
+import RyanHome from './RyanHome';
 import BadWitch from './lesson2/BadWitch';
 import Chess from './lesson3/Chess';
 import RockPaperScissors from './lesson4/RockPaperScissors';
 
 const RyanRoutes = [
-  <Route path="/Ryan" element={<MajHome />} />,
+  <Route path="/Ryan" element={<RyanHome />} />,
   <Route path="1" element={<div>Lesson 1</div>} />,
   <Route path="2" element={<BadWitch />} />,
   <Route path="3" element={<Chess />} />,
