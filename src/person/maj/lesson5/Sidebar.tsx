c04da55fc2f243b45c41e13b854ece22f86dc18d
majicmaj <majd.tarbin@outlook.com>
2025-02-02 08:16:45 -0500

    Refactor Sidebar component to use getUpgrades utility for upgrade management

--- src/person/maj/lesson5/Sidebar.tsx
+++ src/person/maj/lesson5/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import gold_icon from './assets/ui/gold_icon.png';
+import { getUpgrades } from './utils';
 
 interface Sidebar {
   gold: number;
@@ -9,71 +10,14 @@ interface Sidebar {
   setWins: React.Dispatch<React.SetStateAction<number>>;
   play: () => void;
 }
+
 const Sidebar = ({ gold, setGold, setWinrateMod, setLp, play }: Sidebar) => {
-  const upgrades = [
-    {
-      name: 'Watch Pro VOD',
-      cost: 10,
-      description: '+0.1 Skill Mod',
-      buy: () => {
-        setGold((prev) => prev - 10);
-        setWinrateMod((prev) => prev + 0.1);
-      },
-    },
-    {
-      name: 'Gacha Skin',
-      cost: 10,
-      description: '5% chance for +1000 gold',
-      buy: () => {
-        setGold((prev) => prev - 10);
-        if (Math.random() > 0.95) {
-          setGold((prev) => prev + 1000);
-        }
-      },
-    },
-    {
-      name: 'Rage Queue',
-      cost: 10,
-      description: 'Play 10 games in a row',
-      buy: () => {
-        setGold((prev) => prev - 10);
-        for (let i = 0; i < 10; i++) {
-          play();
-        }
-      },
-    },
-
-    {
-      name: 'Get Coaching',
-      cost: 500,
-      description: '+ (0-10) Skill mod',
-      buy: () => {
-        setGold((prev) => prev - 500);
-        setWinrateMod((prev) => prev + Math.random() * 10);
-      },
-    },
-    {
-      name: 'Scripting',
-      cost: 1000,
-      description: '+10 Skill mod',
-      buy: () => {
-        setGold((prev) => prev - 1000);
-        setWinrateMod((prev) => prev + 10);
-      },
-    },
-
-    {
-      name: 'Get boosted',
-      cost: 1000,
-      description: '+100 LP',
-      buy: () => {
-        if (gold >= 1000) {
-          setGold((prev) => prev - 1000);
-          setLp((prev) => prev + 100);
-        }
-      },
-    },
-  ];
+  const upgrades = getUpgrades({
+    setGold,
+    setWinrateMod,
+    setLp,
+    play,
+  });
 
   return (
     <div className="grid w-[300px] gap-4">
--- src/person/maj/lesson5/utils.ts
+++ src/person/maj/lesson5/utils.ts
@@ -32,3 +32,76 @@ export const getFullRank = (lp: number) => {
   const division = getRomanNumeral(getDivision(lp));
   return `${rank} ${division}`;
 };
+
+export const getUpgrades = ({
+  setGold,
+  setWinrateMod,
+  setLp,
+  play,
+}: {
+  setGold: React.Dispatch<React.SetStateAction<number>>;
+  setWinrateMod: React.Dispatch<React.SetStateAction<number>>;
+  setLp: React.Dispatch<React.SetStateAction<number>>;
+  play: () => void;
+}) => [
+  {
+    name: 'Watch Pro VOD',
+    cost: 10,
+    description: '+0.1 Skill Mod',
+    buy: () => {
+      setGold((prev) => prev - 10);
+      setWinrateMod((prev) => prev + 0.1);
+    },
+  },
+  {
+    name: 'Gacha Skin',
+    cost: 10,
+    description: '5% chance for +1000 gold',
+    buy: () => {
+      setGold((prev) => prev - 10);
+      if (Math.random() > 0.95) {
+        setGold((prev) => prev + 1000);
+      }
+    },
+  },
+  {
+    name: 'Rage Queue',
+    cost: 10,
+    description: 'Play 10 games in a row',
+    buy: () => {
+      setGold((prev) => prev - 10);
+      for (let i = 0; i < 10; i++) {
+        play();
+      }
+    },
+  },
+
+  {
+    name: 'Get Coaching',
+    cost: 500,
+    description: '+ (0-10) Skill mod',
+    buy: () => {
+      setGold((prev) => prev - 500);
+      setWinrateMod((prev) => prev + Math.random() * 10);
+    },
+  },
+  {
+    name: 'Scripting',
+    cost: 1000,
+    description: '+10 Skill mod',
+    buy: () => {
+      setGold((prev) => prev - 1000);
+      setWinrateMod((prev) => prev + 10);
+    },
+  },
+
+  {
+    name: 'Get boosted',
+    cost: 1000,
+    description: '+100 LP',
+    buy: () => {
+      setGold((prev) => prev - 1000);
+      setLp((prev) => prev + 100);
+    },
+  },
+];
